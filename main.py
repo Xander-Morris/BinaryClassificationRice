cc10935fbc6400d9074c3e02a745e44b3ba2af27
Xander <87785817+XanderYZZ@users.noreply.github.com>
2026-06-18 17:51:26 -0700

    work

--- main.py
+++ main.py
@@ -7,3 +7,18 @@ import plotly.express as px
 
 pd.options.display.max_rows = 10
 pd.options.display.float_format = "{:.1f}".format
+
+rice_dataset_raw = pd.read_csv("https://download.mlcc.google.com/mledu-datasets/Rice_Cammeo_Osmancik.csv")
+
+rice_dataset = rice_dataset_raw[[
+    'Area',
+    'Perimeter',
+    'Major_Axis_Length',
+    'Minor_Axis_Length',
+    'Eccentricity',
+    'Convex_Area',
+    'Extent',
+    'Class',
+]]
+
+rice_dataset.describe()
