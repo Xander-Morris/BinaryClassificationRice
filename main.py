b30e60a08c0001d2d48f419b025554a2634d97fa
Xander <87785817+XanderYZZ@users.noreply.github.com>
2026-06-18 17:50:14 -0700

    work

---
+++ main.py
@@ -0,0 +1,9 @@
+import keras
+import ml_edu.experiment
+import ml_edu.results
+import numpy as np
+import pandas as pd
+import plotly.express as px
+
+pd.options.display.max_rows = 10
+pd.options.display.float_format = "{:.1f}".format
